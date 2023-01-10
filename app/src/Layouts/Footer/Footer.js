5168a8342db9a071d5d646ed5015d853e400c005
enimad <aminedjellab92@gmail.com>
2023-01-10 01:20:43 +0100

    ajout du corps et style de Header et Footer - Next: Home à finaliser

--- app/src/Layouts/Footer/Footer.js
+++ app/src/Layouts/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import "./Footer.css";
+import logo from '@/Assets/Images/Components/Footer/logo_footer.svg';
 
 const Footer = () => {
     return (
         <div className='footer'>
-            ******* Footer Works ! *******
+            <img className='logo_footer' src={logo} alt='Kasa logo' />
+            <p>© 2020 Kasa. All rights reserved</p>
         </div>
     );
 };
--- app/src/Layouts/Header/Header.js
+++ app/src/Layouts/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Header.css';
+import logo from '@/Assets/Images/Components/Header/logo_header.svg';
 
 const Header = () => {
     return (
-        <div className='header'>
-            ******* Header Works ! *******
-        </div>
+        <header className='header'>
+            <img className='logo_header' src={logo} alt='Kasa logo' />
+            <nav>
+                <ul>
+                    <li>Accueil</li>
+                    <li>A Propos</li>
+                </ul>
+            </nav>
+        </header>
     );
 };
 
