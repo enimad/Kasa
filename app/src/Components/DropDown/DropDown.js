178533c164f5500ff344fb3ef29cc826c64ca921
enimad <aminedjellab92@gmail.com>
2023-01-17 01:35:56 +0100

    Page Apropos: component DropDown créé, page terminée, tout fonctionne > Next: Page logement/:id

---
+++ app/src/Components/DropDown/DropDown.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+import "@/Components/DropDown/DropDown.css";
+
+function DropDown({ title, image, content }) {
+    const [dropDown, setDropDown] = useState(false)
+
+    const classToggle = () => {
+        setDropDown(!dropDown);
+    }
+
+    return (
+        <li className='dropdown'>
+            <div className='dropdown-titre-wrapper'>
+                <h3 className='dropdown-titre'>{title}</h3>
+                <img className={`dropdown-img ${dropDown ? "rotate" : ""}`} src={image} alt='Arrow' onClick={classToggle}></img>
+            </div>
+            <p className={`dropdown-content ${dropDown ? "" : "displayNone"}`}>{content}</p>
+        </li>
+    );
+};
+
+export default DropDown;
